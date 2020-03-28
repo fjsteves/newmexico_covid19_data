36dc6eb75cfa82fc047bd78a7fa7e7465b5ba77e
Frank Steves <fsteves@getclear.net>
2020-03-28 17:01:28 -0600

    Minor dataset and parsing corrections related to day/date

--- parse_data.py
+++ parse_data.py
@@ -22,22 +22,22 @@ def plotData(df):
     fig, ax = plt.subplots(figsize=(10,6))
 
     filtered_df = df.query('county == "Bernalillo County"')
-    filtered_df['day'] = pd.to_datetime(filtered_df['day'])
+    filtered_df['date'] = pd.to_datetime(filtered_df['date'])
     #print (filtered_df)
 
     #Total Bernco Cases
-    sns.lineplot(x='day',y='case_count', data=filtered_df, label='Bernalillo Cases', ax=ax, lw=2)
-    sns.lineplot(x='day',y='death_count', data=filtered_df, label='Bernalillo Deaths', ax=ax, lw=2)
+    sns.lineplot(x='date',y='case_count', data=filtered_df, label='Bernalillo Cases', ax=ax, lw=2)
+    sns.lineplot(x='date',y='death_count', data=filtered_df, label='Bernalillo Deaths', ax=ax, lw=2)
     
     nm_df = df
     
-    nm_df_casesums = (nm_df.groupby(['day'])['case_count'].sum()).reset_index()
-    nm_df_deathsums = (nm_df.groupby(['day'])['death_count'].sum()).reset_index()
+    nm_df_casesums = (nm_df.groupby(['date'])['case_count'].sum()).reset_index()
+    nm_df_deathsums = (nm_df.groupby(['date'])['death_count'].sum()).reset_index()
 
-    nm_df_casesums['day'] = pd.to_datetime(nm_df_casesums['day'])
-    nm_df_deathsums['day'] = pd.to_datetime(nm_df_deathsums['day'])
+    nm_df_casesums['date'] = pd.to_datetime(nm_df_casesums['date'])
+    nm_df_deathsums['date'] = pd.to_datetime(nm_df_deathsums['date'])
 
-    nm_df_casesums = nm_df_casesums.set_index(pd.DatetimeIndex(nm_df_casesums['day']))
+    nm_df_casesums = nm_df_casesums.set_index(pd.DatetimeIndex(nm_df_casesums['date']))
     
     #TODO: Create a LOG/EXPO curve.
     #startDate = '2020-03-11'
@@ -49,15 +49,15 @@ def plotData(df):
 
     #Total NM Cases
     #sns.lineplot(x='index' ,y='case_count', data=nm_df_casesums_smooth.reset_index(), label='New Mexico Cases', ax=ax, lw=2)
-    #nm_casedays = np.array(list(range(0, 18)))
+    #nm_casedates = np.array(list(range(0, 18)))
 
     #model = LorentzianModel()
-    #params = model.guess(nm_df_casesums['case_count'], x=nm_casedays)
-    #result = model.fit(nm_df_casesums['case_count'], params, x=nm_casedays)
+    #params = model.guess(nm_df_casesums['case_count'], x=nm_casedates)
+    #result = model.fit(nm_df_casesums['case_count'], params, x=nm_casedates)
 
     #print (result)
-    sns.lineplot(x='day' ,y='case_count', data=nm_df_casesums, label='New Mexico Cases', ax=ax, lw=2)
-    sns.lineplot(x='day',y='death_count', data=nm_df_deathsums, label='New Mexico Deaths', ax=ax, lw=2)
+    sns.lineplot(x='date' ,y='case_count', data=nm_df_casesums, label='New Mexico Cases', ax=ax, lw=2)
+    sns.lineplot(x='date',y='death_count', data=nm_df_deathsums, label='New Mexico Deaths', ax=ax, lw=2)
 
     plt.ylabel('Units', fontsize=14)
     plt.xlabel('Date', fontsize=14)
